cb7d8e970ca72870f17cc6106aeb3f615d1adabf
ayng <agdnyn@gmail.com>
2022-09-06 04:33:00 +1000

    Draft script for scraping data from domain

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -13,20 +13,20 @@ from collections import defaultdict
 
 # user packages
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+import requests
 
 # constants
 BASE_URL = "https://www.domain.com.au"
-N_PAGES = range(1, 51) # update this to your liking
+N_PAGES = range(1, 20) # update this to your liking
 
 # begin code
 url_links = []
 property_metadata = defaultdict(dict)
-
+headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
 # generate list of urls to visit
 for page in N_PAGES:
     url = BASE_URL + f"/rent/melbourne-region-vic/?sort=price-desc&page={page}"
-    bs_object = BeautifulSoup(urlopen(url), "lxml")
+    bs_object = BeautifulSoup(requests.get(url, headers=headers).text, "html.parser")
 
     # find the unordered list (ul) elements which are the results, then
     # find all href (a) tags that are from the base_url website.
@@ -44,12 +44,9 @@ for page in N_PAGES:
         # if its a property address, add it to the list
         if 'address' in link['class']:
             url_links.append(link['href'])
-
-
 # for each url, scrape some basic metadata
 for property_url in url_links[1:]:
-    bs_object = BeautifulSoup(urlopen(property_url), "lxml")
-
+    bs_object = BeautifulSoup(requests.get(property_url, headers=headers).text, "html.parser")
     # looks for the header class to get property name
     property_metadata[property_url]['name'] = bs_object \
         .find("h1", {"class": "css-164r41r"}) \
@@ -75,7 +72,7 @@ for property_url in url_links[1:]:
     ]
 
     property_metadata[property_url]['rooms'] = [
-        re.findall(r'\d\s[A-Za-z]+', feature.text)[0] for feature in bs_object \
+        re.findall(r'\d\s[A-Za-z]+', feature.text) for feature in bs_object \
             .find("div", {"data-testid": "property-features"}) \
             .findAll("span", {"data-testid": "property-features-text-container"})
     ]
@@ -85,5 +82,6 @@ for property_url in url_links[1:]:
         .strip('</p>')
 
 # output to example json in data/raw/
-with open('../data/raw/example.json', 'w') as f:
+with open('../data/raw/scraped_rental.json', 'w') as f:
     dump(property_metadata, f)
+
